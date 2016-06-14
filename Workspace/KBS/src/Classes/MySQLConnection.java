38a61e48d521a3ed3ea77e87753b0d12e6f3c492
Burim Cakolli <burim.cakolli@hotmail.com>
2016-06-14 11:22:29 +0200

    Treiber hinzugefügt

---
+++ Workspace/KBS/src/Classes/MySQLConnection.java
@@ -0,0 +1,71 @@
+package Classes;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+ 
+public class MySQLConnection{
+private static Connection con = null;
+private static String dbHost = "localhost"; // Hostname
+private static String dbPort = "3306";      // Port -- Standard: 3306
+private static String dbName = "kinobuchdb";   // Datenbankname
+private static String dbUser = "root";     // Datenbankuser
+private static String dbPass = "";      // Datenbankpasswort
+ 
+	private MySQLConnection(){
+	    try {
+	        Class.forName("com.mysql.jdbc.Driver"); // Datenbanktreiber für JDBC Schnittstellen laden.
+	 
+	        // Verbindung zur JDBC-Datenbank herstellen.
+	        con = DriverManager.getConnection("jdbc:mysql://"+dbHost+":"+ dbPort+"/"+dbName+"?"+"user="+dbUser+"&"+"password="+dbPass);
+	    } catch (ClassNotFoundException e) {
+	        System.out.println("Treiber nicht gefunden");
+	    } catch (SQLException e) {
+	        System.out.println("Verbindung nicht moglich");
+	        System.out.println("SQLException: " + e.getMessage());
+	        System.out.println("SQLState: " + e.getSQLState());
+	        System.out.println("VendorError: " + e.getErrorCode());
+	    }//-catch
+	 }//-MySQLConnection
+ 
+	private static Connection getInstance(){
+	    if(con == null)
+	        new MySQLConnection();
+	    return con;
+	}//-getInstance
+ 
+  //Gebe Tabelle in die Konsole aus
+  public static void printNameList(){
+      con = getInstance();
+ 
+      if(con != null){
+      // Abfrage-Statement erzeugen.
+      Statement query;
+      try {
+          query = con.createStatement();
+ 
+          // Tabelle anzeigen
+          String sql =
+                "SELECT * FROM tbl_Saal";
+          ResultSet result = query.executeQuery(sql);
+ 
+        // Ergebnisstabelle durchforsten
+          while (result.next()) {
+          String beschreibung = result.getString("Beschreibung");       
+          String info = beschreibung;
+         
+          System.out.println(info);
+          }//-while
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }//-catch
+    }//-if
+  }//-printNameList
+  
+  public static void main(String[] args){
+	  MySQLConnection.printNameList();
+  }//-main
+  
+}//-classes
