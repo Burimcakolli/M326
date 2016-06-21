8b9e542e8e00e9d19162b2269d8dac1d484feb63
Burim Cakolli <burim.cakolli@hotmail.com>
2016-06-21 10:18:51 +0200

    Variablen hinzf. bei Saal

--- Workspace/KBS/src/Classes/Saal.java
+++ Workspace/KBS/src/Classes/Saal.java
@@ -1,7 +1,20 @@
+/*******************
+ * @author Burim & Toshiki
+ * Erstellt: 21.06.2016 
+ * Auftrag : TBZ 
+ *******************/
 package Classes;
+import java.util.ArrayList;
 
 public class Saal {
 
+//--Instanzvariablen
+	private int id;
+	private String beschreibung;
+	private ArrayList<Sitz> sitzplaetze;
+//--END Instanzvariablen
+	
+//--Konstruktoren
 	Saal(int x, int y){
 		
 	}//-Contstructor neu in Tabelle
@@ -9,6 +22,9 @@ public class Saal {
 	Saal(int id){
 		
 	}//-Constructor neu in Programm von Tabelle
+//--END Konstruktoren
+	
+	
 	
 	
 }//-Saal
